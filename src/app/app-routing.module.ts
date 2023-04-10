abfd6e93c73198193d153090b2195f59ff1e928e
Prottoy Ghosh <ghoshprottoy98@gmail.com>
2023-04-10 11:34:07 +0600

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerSupportComponent } from './customer-support/customer-support.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [ { path: '', pathMatch: 'full', redirectTo: 'chat-with-support-bot' },]
+;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routedComponents = []
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Configuration, OpenAIApi} from "openai";
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'chatbot';
+
+  response: any;
+
+  ngOnInit(): void {
+    //this.tryCall();
+  }
+  
 }
